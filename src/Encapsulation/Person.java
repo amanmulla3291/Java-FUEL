077b00bc3301215e348d19b7d33bb61cc377fd07
amanmulla3291 <aamulla@siberindia.edu.in>
2025-03-10 16:12:05 +0530

    Encapsulation

---
+++ src/Encapsulation/Person.java
@@ -0,0 +1,11 @@
+package Encapsulation;
+
+public class Person {
+    private String Name;
+    public void SetName(String name){
+        this.Name=name;
+    }
+    public String GetName(){
+        return Name;
+    }
+}
---
+++ src/Encapsulation/Test.java
@@ -0,0 +1,9 @@
+package Encapsulation;
+
+public class Test {
+    public static void main(String[] args) {
+        Person p1 = new Person();
+        p1.SetName("I'm Sam");
+        System.out.println(p1.GetName());
+    }
+}
