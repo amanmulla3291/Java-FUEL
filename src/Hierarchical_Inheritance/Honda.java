7332c13da5fd691b9e54d554442458847dd755be
amanmulla3291 <aamulla@siberindia.edu.in>
2025-03-11 10:46:14 +0530

    Hierarchical_Inheritance

--- src/Hierarchical_Inheritance/Honda.java
+++ src/Hierarchical_Inheritance/Honda.java
@@ -14,6 +14,30 @@ public class Honda {
         this.price = price;
     }
     void Display(){
+        System.out.println("Brand:" + brand + ", Engine:" + engineType + ", Speed:" + topSpeed + " km/h, Mileage:" + mileage + " km/l, Price: $" + price);
+    }
+}
 
+class Bike extends Honda{
+    Bike(){
+        super("Petrol",180, 40.0, 1500);
+    }
+}
+
+class Car extends Honda{
+    Car(){
+        super("Diesel",220, 15.0, 20000);
+    }
+}
+
+class Scooter extends Honda{
+    Scooter(){
+        super("Electric",80, 60.0, 1200);
+    }
+}
+
+class Truck extends Honda{
+    Truck(){
+        super("Diesel",120, 5.0, 50000);
     }
 }
---
+++ src/Hierarchical_Inheritance/Test.java
@@ -0,0 +1,10 @@
+package Hierarchical_Inheritance;
+
+public class Test {
+    public static void main(String[] args) {
+        new Bike().Display();
+        new Car().Display();
+        new Scooter().Display();
+        new Truck().Display();
+    }
+}
