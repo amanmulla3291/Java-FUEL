7f8343c12714484fdca1c4e0c0dc298fb04cdb7f
amanmulla3291 <aamulla@siberindia.edu.in>
2025-03-17 12:57:22 +0530

    Collection Framework contd.

--- README.md
+++ README.md
@@ -503,4 +503,3 @@ public class Demo {
     }
 }
 ```
-
--- src/ArrayList/Demo.java
+++ src/ArrayList/Demo.java
@@ -11,6 +11,35 @@ public class Demo {
         al.add(20.5);
         al.add(null);
         al.add(20);
+
+        /* Array List. */
         System.out.println("Array List : " + al);
+        System.out.println("------------------------------------------");
+        /* Size of Array List. */
+        System.out.println("Size of Array List: " + al.size());
+        System.out.println("------------------------------------------");
+        /* Access data by index. */
+        System.out.println(al.get(2));
+        System.out.println(al.get(4));
+        System.out.println("------------------------------------------");
+        /* Access data by contains()
+         * To check the data is present or not.
+         * */
+        System.out.println("al.contains(null) " + al.contains(null));
+        System.out.println(al.contains("SAM"));
+
+        /* Deleting data using delete()
+         * To remove data present in Array List using index value.
+         * */
+        System.out.println(al.remove(3));
+        System.out.println(al);
+
+        /* Access data by contains()
+         * To check the data is present or not.
+         * */
+        System.out.println(al.isEmpty());
+        al.clear();
+        System.out.println(al);
+        System.out.println(al.isEmpty());
     }
 }
